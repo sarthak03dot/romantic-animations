8ecaa47de53bd39bcea80cd37a951d5ad63e6b6c
Sarthak Singh <sarthakankshawebjvb@gmail.com>
2026-05-23 21:22:23 +0530

    setup github pages deploy

--- vite.demo.config.js
+++ vite.demo.config.js
@@ -5,7 +5,7 @@ import { readFileSync } from 'fs';
 const pkg = JSON.parse(readFileSync(new URL('./package.json', import.meta.url), 'utf-8'));
 
 export default defineConfig({
-  base: './', // Ensures assets are loaded with relative paths, perfect for GitHub Pages
+  base: '/romantic-animations/', // Ensures assets are loaded with relative paths, perfect for GitHub Pages
   define: {
     __APP_VERSION__: JSON.stringify(pkg.version),
   },
